e0d7067be55020c63e7e4875eb59693920b89b91
linga neharica <lneharica96@gmail.com>
2017-07-16 10:27:46 +0530

    Version1 added notifications

--- Final/sendnotifications.php
+++ Final/sendnotifications.php
@@ -11,15 +11,21 @@ $sid = "ACbaf7dfd53afd8edb78530850970c2def";
 $token = "f6ac6a2ca00695f074936319b30dc600";
 ;
 $client = new Client($sid, $token);
-
+$i = 3;
 // Use the client to do fun stuff like send text messages!
-$client->messages->create(
+while($i>0)
+{
+	$client->messages->create(
     // the number you'd like to send the message to
         "+917207805474",
     array(
         // A Twilio phone number you purchased at twilio.com/console
         'from' => '+16264270273',
         // the body of the text message you'd like to send
-        'body' => "Hey Jenny! Good luck on the bar exam!"
+        'body' => "A gentle reminder for your examination. "
     )
 );
+sleep(5);
+$i = $i-1;
+}
+?>
